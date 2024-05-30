462a9355c2fd7e6be6e474ae5dee69be343452d9
Xuehang Cang <xuehangcang@outlook.com>
2024-05-31 00:48:47 +0800

    update:vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base:"/xuehangcang.github.io/",
+  base:"/",
   plugins: [
     vue(),
   ],
