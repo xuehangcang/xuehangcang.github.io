ffc3e2199708acbe18e5da401458f7a57f894ebd
xuehangcang <xuehangcang@users.noreply.github.com>
2024-05-31 06:37:53 +0000

    deploy: 666a1b8d48d989de444bc41a8f7d4320c85ec04b

--- assets/about-uvnnx1Ap.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as s,a as t}from"./index-B7TTI2Ju.js";const a={},c={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return s(),o("div",c,_)}const u=e(a,[["render",r]]);export{u as default};
