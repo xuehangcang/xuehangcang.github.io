92c7149c91c953eacd3134a08e51dd82ab626b27
xuehangcang <xuehangcang@icloud.com>
2024-05-31 15:31:03 +0800

    update:index

--- src/views/index.vue
+++ src/views/index.vue
@@ -11,7 +11,7 @@
         </div>
 
         <p class="italic mb-4">
-            <a class="text-blue-700" href="https://sebsauvage.net/python/" target="_blank">"Life is short You need Python"</a>
+            <a class="text-blue-700" href="https://sebsauvage.net/python/" target="_blank">"Life is short. You need Python."</a>
         </p>
 
         <p class="text-2xl font-semibold mb-2">
@@ -19,11 +19,11 @@
         </p>
 
         <p class="mb-4">
-            I'm a <a class="text-blue-700" href="https://www.python.org" target="_blank">Python</a>  programming language enthusiast from  <a class="text-blue-700" href="https://en.wikipedia.org/wiki/China" target="_blank">China 🇨🇳</a>. See also my <a class="text-blue-700" href="#">resume</a> and my  <RouterLink to="/pics" class="text-blue-700">publications list</RouterLink>, a <a class="text-blue-700" href="#">brief bio</a>, assorted <a class="text-blue-700" href="#">writings</a>, <a class="text-blue-700" href="#">presentations</a>, some <a class="text-blue-700" href="#">pictures of me</a>, I am <a class="text-blue-700" href="https://twitter.com/xuehangcang" target="_blank">@Xuehang Cang</a> on Twitter.
+            I'm a <a class="text-blue-700" href="https://www.python.org" target="_blank">Python</a>  programming language enthusiast from  <a class="text-blue-700" href="https://en.wikipedia.org/wiki/China" target="_blank">China 🇨🇳</a>. See also my <a class="text-blue-700" href="#">resume</a>, and my  <RouterLink to="/pics" class="text-blue-700">publications list</RouterLink>, a <a class="text-blue-700" href="#">brief bio</a>, assorted <a class="text-blue-700" href="#">writings</a>, <a class="text-blue-700" href="#">presentations</a>,and some <RouterLink to="/pics" class="text-blue-700">pictures of me</RouterLink>. I am <a class="text-blue-700" href="https://twitter.com/xuehangcang" target="_blank">@Xuehang Cang</a> on Twitter.
 
         </p>
         <p class="mb-4">
-            I'm learning something about <a class="text-blue-700" href="https://en.wikipedia.org/wiki/Machine_learning" target="_blank">machine learning</a> , Currently I’m interested in <a class="text-blue-700" href="https://en.wikipedia.org/wiki/Computer_vision" target="_blank">computer vision</a>, <a class="text-blue-700" href="https://huggingface.co/tasks/text-generation">text generation (LLMs)</a> and <a class="text-blue-700" href="https://huggingface.co/blog/vlms">vision-language models</a>.
+            I'm learning something about <a class="text-blue-700" href="https://en.wikipedia.org/wiki/Machine_learning" target="_blank">machine learning</a>. Currently I’m interested in <a class="text-blue-700" href="https://en.wikipedia.org/wiki/Computer_vision" target="_blank">computer vision</a>, <a class="text-blue-700" href="https://huggingface.co/tasks/text-generation">text generation (LLMs)</a> and <a class="text-blue-700" href="https://huggingface.co/blog/vlms">vision-language models</a>.
         </p>
 
         <p class="text-2xl font-semibold mb-2">
@@ -31,8 +31,7 @@
         </p>
 
         <p class="mb-4">
-            You can send email for me to <a class="text-blue-700" href="mailto:xuehangcang@outlook.com">xuehangcang@outlook.com</a>.
-
+            You can send me an email at <a class="text-blue-700" href="mailto:xuehangcang@outlook.com">xuehangcang@outlook.com</a>.
         </p>
 
         <p class="text-2xl font-semibold mb-2">
@@ -40,11 +39,11 @@
         </p>
 
         <p class="mb-4">
-            The <a class="text-blue-700" href="https://baike.baidu.com/item/%E8%8B%8D%E5%A7%93" target="_blank">surname Cang</a>, pronounced as cāng (ㄘㄤ), originates from the Manchu ethnicity. It traces back to the Jurchen tribes during the Ming Dynasty, and it belongs to the category of surnames that were sinicized from tribal names.
+            The <a class="text-blue-700" href="https://baike.baidu.com/item/%E8%8B%8D%E5%A7%93" target="_blank">surname Cang</a>, pronounced as cāng (ㄘㄤ), originates from the Manchu ethnic group. It traces back to the Jurchen tribes during the Ming Dynasty, and it belongs to the category of surnames that were Sinicized from tribal names.
         </p>
 
         <p class="mb-4">
-            The Manchu <a class="text-blue-700" href="https://baike.baidu.com/item/%E8%90%A8%E5%85%8B%E8%BE%BE%E6%B0%8F" target="_blank">Sakda clang</a>, originating from the <a class="text-blue-700" href="https://baike.baidu.com/item/%E5%B7%B4%E9%9B%85%E6%8B%89%E6%B0%8F" target="_blank">Bayara clan</a>, is a branch of the <a class="text-blue-700" href="https://baike.baidu.com/item/%E8%A7%89%E7%BD%97%E6%B0%8F" target="_blank">Jueluo clan</a>. The clan name was later derived from geographical locations. In Manchu, it is called "Sakda Hala," which translates to "苍老" in Chinese. The clan historically resided in areas such as Ningguta, Ahur River, Yehe, Sakda, Hetu Ala, Namudulu, Deliwohe, and Heilongjiang, among others.
+            The Manchu <a class="text-blue-700" href="https://baike.baidu.com/item/%E8%90%A8%E5%85%8B%E8%BE%BE%E6%B0%8F" target="_blank">Sakda clan</a>, originating from the <a class="text-blue-700" href="https://baike.baidu.com/item/%E5%B7%B4%E9%9B%85%E6%8B%89%E6%B0%8F" target="_blank">Bayara clan</a>, is a branch of the <a class="text-blue-700" href="https://baike.baidu.com/item/%E8%A7%89%E7%BD%97%E6%B0%8F" target="_blank">Jueluo clan</a>. The clan name was later derived from geographical locations. In Manchu, it is called "Sakda Hala," which translates to "苍老" in Chinese. The clan historically resided in areas such as Ningguta, Ahur River, Yehe, Sakda, Hetu Ala, Namudulu, Deliwohe, and Heilongjiang, among others.
         </p>
 
         <p class="text-2xl font-semibold mb-2">
@@ -53,11 +52,20 @@
 
         <ul class="list-disc list-inside mb-4">
             <li class="mb-2">
+                Here is the <a class="text-blue-500" href="https://github.com/xuehangcang" target="_blank">project and python packages</a> I have released.
+            </li>
+
+            <li class="mb-2">
                 Here is the  <a class="text-blue-500" href="https://huggingface.co/Xuehang" target="_blank">model and dataset</a> I have released, which includes some test data.
             </li>
+
             <li class="mb-2">
                 Here is the <a class="text-blue-500" href="https://www.heywhale.com/home/user/profile/5ee9f592c1914600362bea45/overview" target="_blank">article and tutorial</a> I have released.
             </li>
+
+
+
+
         </ul>
 
         <hr class="my-4">
