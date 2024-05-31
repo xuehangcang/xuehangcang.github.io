6209cf5714eb88d2991172dcb159ce1aeed34752
xuehangcang <xuehangcang@icloud.com>
2024-05-31 14:24:45 +0800

    update:index,pics

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import index from '../views/index.vue'
+import pics from '../views/pics.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'index',
+      component: index
+    },
+    {
+      path: '/pics',
+      name: 'pics',
+      component: pics
     },
     {
       path: '/about',
@@ -15,7 +20,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/about.vue')
     }
   ]
 })
---
+++ src/views/pics.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="container mx-auto p-4 bg-white text-black">
+        <h1 class="text-3xl font-bold mb-4">Xuehang Cang (苍学航) - Pictures of me</h1>
+        <p><a href="/" class="text-blue-500 hover:underline">Back to my personal home page</a></p>
+
+        <div class="mt-4">
+            <a href="../assets/image/2021.jpg">
+                <img class="w-64 h-auto" src="../assets/image/2021.jpg" alt="xuehang 2021">
+            </a>
+            <p>2021.</p>
+        </div>
+
+        <div class="mt-4">
+            <a href="../assets/image/2018.jpg">
+                <img class="w-64 h-auto" src="../assets/image/2018.jpg" alt="xuehang 2018">
+            </a>
+            <p>2018.</p>
+        </div>
+
+        <div class="mt-4">
+            <a href="../assets/image/2017-2.jpg">
+                <img class="w-64 h-auto" src="../assets/image/2017-2.jpg" alt="xuehang 2017">
+            </a>
+            <p>2017.</p>
+        </div>
+
+
+        <div class="mt-4">
+            <a href="../assets/image/2017-1.jpg">
+                <img class="w-64 h-auto" src="../assets/image/2017-1.jpg" alt="xuehang 2017">
+            </a>
+            <p>2017.</p>
+        </div>
+
+        <div class="mt-4">
+            <a href="../assets/image/2013-2.jpg">
+                <img class="w-64 h-auto" src="../assets/image/2013-2.jpg" alt="xuehang 2013">
+            </a>
+            <p>2013.</p>
+        </div>
+
+        <div class="mt-4">
+            <a href="../assets/image/2013-1.jpg">
+                <img class="w-64 h-auto" src="../assets/image/2013-1.jpg" alt="xuehang 2013">
+            </a>
+            <p>2013.</p>
+        </div>
+
+        <div class="mt-4">
+            <a href="../assets/image/2012.jpeg">
+                <img class="w-64 h-auto" src="../assets/image/2012.jpeg" alt="xuehang 2012">
+            </a>
+            <p>2012.</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
