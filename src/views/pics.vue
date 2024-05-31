666a1b8d48d989de444bc41a8f7d4320c85ec04b
xuehangcang <xuehangcang@icloud.com>
2024-05-31 14:37:29 +0800

    debug:图片点击无法跳转

--- src/views/pics.vue
+++ src/views/pics.vue
@@ -10,52 +10,38 @@
         </p>
 
         <div class="mt-4">
-            <a href="../assets/image/2021.jpg">
-                <img class="w-64 h-auto" src="../assets/image/2021.jpg" alt="xuehang 2021">
-            </a>
+            <img class="w-64 h-auto" src="../assets/image/2021.jpg" alt="xuehang 2021">
             <p>2021.</p>
         </div>
 
         <div class="mt-4">
-            <a href="../assets/image/2018.jpg">
-                <img class="w-64 h-auto" src="../assets/image/2018.jpg" alt="xuehang 2018">
-            </a>
+            <img class="w-64 h-auto" src="../assets/image/2018.jpg" alt="xuehang 2018">
             <p>2018.</p>
         </div>
 
         <div class="mt-4">
-            <a href="../assets/image/2017-2.jpg">
-                <img class="w-64 h-auto" src="../assets/image/2017-2.jpg" alt="xuehang 2017">
-            </a>
+            <img class="w-64 h-auto" src="../assets/image/2017-2.jpg" alt="xuehang 2017">
             <p>2017.</p>
         </div>
 
 
         <div class="mt-4">
-            <a href="../assets/image/2017-1.jpg">
-                <img class="w-64 h-auto" src="../assets/image/2017-1.jpg" alt="xuehang 2017">
-            </a>
+            <img class="w-64 h-auto" src="../assets/image/2017-1.jpg" alt="xuehang 2017">
             <p>2017.</p>
         </div>
 
         <div class="mt-4">
-            <a href="../assets/image/2013-2.jpg">
-                <img class="w-64 h-auto" src="../assets/image/2013-2.jpg" alt="xuehang 2013">
-            </a>
+            <img class="w-64 h-auto" src="../assets/image/2013-2.jpg" alt="xuehang 2013">
             <p>2013.</p>
         </div>
 
         <div class="mt-4">
-            <a href="../assets/image/2013-1.jpg">
-                <img class="w-64 h-auto" src="../assets/image/2013-1.jpg" alt="xuehang 2013">
-            </a>
+            <img class="w-64 h-auto" src="../assets/image/2013-1.jpg" alt="xuehang 2013">
             <p>2013.</p>
         </div>
 
         <div class="mt-4">
-            <a href="../assets/image/2012.jpeg">
-                <img class="w-64 h-auto" src="../assets/image/2012.jpeg" alt="xuehang 2012">
-            </a>
+            <img class="w-64 h-auto" src="../assets/image/2012.jpeg" alt="xuehang 2012">
             <p>2012.</p>
         </div>
     </div>
