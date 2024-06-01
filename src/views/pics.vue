3fcc201988994a0579850eb360a23fded4ccb9d5
xuehangcang <xuehangcang@icloud.com>
2024-06-01 18:24:46 +0800

    update:pics

--- src/views/pics.vue
+++ src/views/pics.vue
@@ -11,38 +11,38 @@
 
         <div class="mt-4">
             <img class="w-64 h-auto" src="../assets/image/2021.jpg" alt="xuehang 2021">
-            <p>2021.</p>
+            <p>2021. Changsha, China</p>
         </div>
 
         <div class="mt-4">
             <img class="w-64 h-auto" src="../assets/image/2018.jpg" alt="xuehang 2018">
-            <p>2018.</p>
+            <p>2018. Jixi, China</p>
         </div>
 
         <div class="mt-4">
             <img class="w-64 h-auto" src="../assets/image/2017-2.jpg" alt="xuehang 2017">
-            <p>2017.</p>
+            <p>2017. Hangzhou, China</p>
         </div>
 
 
         <div class="mt-4">
             <img class="w-64 h-auto" src="../assets/image/2017-1.jpg" alt="xuehang 2017">
-            <p>2017.</p>
+            <p>2017. Xiamen, China</p>
         </div>
 
         <div class="mt-4">
             <img class="w-64 h-auto" src="../assets/image/2013-2.jpg" alt="xuehang 2013">
-            <p>2013.</p>
+            <p>2013. Dalian, China</p>
         </div>
 
         <div class="mt-4">
             <img class="w-64 h-auto" src="../assets/image/2013-1.jpg" alt="xuehang 2013">
-            <p>2013.</p>
+            <p>2013. Dalian, China</p>
         </div>
 
         <div class="mt-4">
             <img class="w-64 h-auto" src="../assets/image/2012.jpeg" alt="xuehang 2012">
-            <p>2012.</p>
+            <p>2012. Река Суйфун, Китай</p>
         </div>
     </div>
 </template>
