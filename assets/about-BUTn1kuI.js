f772b6e7e617eafa5c8929055995262eb81d1e17
xuehangcang <xuehangcang@users.noreply.github.com>
2024-06-01 22:53:43 +0000

    deploy: 813698d6b3f76ec61c598a4c6ad0dfd8b6e0c02a

---
+++ assets/about-BUTn1kuI.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as s,a as t}from"./index-Djz9YEax.js";const a={},c={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return s(),o("div",c,_)}const u=e(a,[["render",r]]);export{u as default};
